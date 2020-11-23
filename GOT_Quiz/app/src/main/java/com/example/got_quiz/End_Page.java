7a20ea995537904c289390923d27790cd9f95f9e
Hakim Ahmadi <hakimsshah@gmail.com>
2020-11-23 12:11:00 +0600

    Quiz_app is finalized...

---
+++ GOT_Quiz/app/src/main/java/com/example/got_quiz/End_Page.java
@@ -0,0 +1,37 @@
+package com.example.got_quiz;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class End_Page extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_end__page);
+
+        Button playAgain = findViewById(R.id.PlayAgainbtn);
+        Button endGame = findViewById(R.id.EndGamebtn);
+
+        playAgain.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                playGame();
+            }
+        });
+        endGame.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                System.exit(0);
+            }
+        });
+    }
+    public void playGame(){
+        Intent intent = new Intent(this, QuizBody.class);
+        startActivity(intent);
+    }
+}
